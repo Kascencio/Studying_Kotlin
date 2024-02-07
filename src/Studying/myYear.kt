2618b9d55b15c4c99bad7ef59d9900f729542dda
Kascencio <keduardoascencio@gmail.com>
2024-02-06 21:45:33 -0600

    New

---
+++ src/Studying/myYear.kt
@@ -0,0 +1,10 @@
+package Studying
+
+    fun main(){
+        var ageYear: Int = 2004
+        var yearActually: Int = 2024
+        var age = yearActually - ageYear
+        println("Mi edad: $age")
+    }
+
+}
